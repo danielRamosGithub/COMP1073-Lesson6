bd65f7aa395edf4caed2429d4c63d3cb898a88be
Daniel Ramos <danielramos.dcr@gmail.com>
2017-02-15 13:07:08 -0500

    JSON file, inject data into the html.

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,46 +5,46 @@ console.log("App Started");
 
 // IIFE
 (function() {
+    let data = {
+        "games": [
+            {
+                "name": "Fallout 4",
+                "cost": 69.99,
+                "rating": 4.3
+            },
+            {
+                "name": "Overwatch",
+                "cost": 49.99,
+                "rating": 5.0
+            },
+            {
+                "name": "Diablo 3",
+                "cost": 79.99,
+                "rating": 4.5
+            }
+        ]
+    };
+
+    console.log(`First games: ${data.games[0].name}`);
+
     /* INTERPOLATION */
-    console.info(`Page title: ${document.title}`); 
+    // console.info(`Page title: ${document.title}`); 
     /* --------------------------------------------- */
     if(document.title == "Home") {
-    // make a reference to H1 element on the index page
-    // initialize my firstHeading variable
-    let firstHeading = document.getElementById("firstH1");
-
-    firstHeading.style.color = "#0000ff";
-    firstHeading.style.fontWeight = "300";
-    firstHeading.style.fontStyle = "italic";
-
-    // i can use my firstHeading variable as if it Is the element
-    // console.log(firstHeading.textContent);
-
-    // firstHeading.textContent = "bla bla bla";
-
-    // 3 steps of injecting content to the page
-    // Step 1 - create a reference to an ELEMENT
-    let firstParagraph = document.getElementById("firstParagraph");
-    // Step 2 - Create a variable that contains your content
-    let myContent = "It was a sunny day in Florida. It felt great...";
-    // Step 3 - Assign the variable with your content to the text content property of the reference variable
-    firstParagraph.textContent = myContent;
-
-    let secondParagraph = document.getElementById("secondParagraph");
-    let myHtmlContent = "<h2>Second Heading</h2>" 
-                        + "<p>This is an inner paragraph to the second paragraph</p>";
-    secondParagraph.innerHTML = myHtmlContent;                    
-
-    // create a reference to the button on the page index.html
-    let clickMeButton = document.getElementById("clickMeButton");
-
-    // add an event listener - for the click eeent and call the clickMeButton_Click function 
-    clickMeButton.addEventListener("click", clickMeButton_Click);
-
-    // click function - used as an event handler
-    function clickMeButton_Click() {
-        console.log("clicked");
-    }
+        let gameListBody = document.getElementById("gameListBody");
+        //
+        data.games.forEach(function(game) {
+            // inject "template row" inside the dataRows div tag
+            let newRow = document.createElement("tr");
+            newRow.innerHTML = `
+                <tr>
+                    <td>${game.name}</td>
+                    <td class="text-center">${game.cost}</td>
+                    <td class="text-center">${game.rating}</td>
+                </tr>
+            `;
+            gameListBody.appendChild(newRow);
+        }, this);
     } // end if Home
     else if (document.title == "Project") {
        
